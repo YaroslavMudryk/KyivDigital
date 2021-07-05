c734bbe08e5aeabf558505abd73457aa2e5b836d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-07-05 20:38:45 +0300

    Init project

---
+++ KyivDigital/KyivDigitalMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KyivDigitalMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
