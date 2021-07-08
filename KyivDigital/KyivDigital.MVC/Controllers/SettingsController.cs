0416c641fb8c583b85833b8a2cfe4a27c814a030
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-07-08 23:31:43 +0300

    Added settings controller and page

---
+++ KyivDigital/KyivDigital.MVC/Controllers/SettingsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace KyivDigital.MVC.Controllers
+{
+    [Route("settings")]
+    public class SettingsController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
