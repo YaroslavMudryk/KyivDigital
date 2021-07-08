0fc7102f903ccb7cab97fe313ad06d6eedacdc21
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-07-08 21:52:08 +0300

    Uncommented Logout

--- KyivDigital/KyivDigital.Business/Models/BaseResponse.cs
+++ KyivDigital/KyivDigital.Business/Models/BaseResponse.cs
@@ -1,11 +1,16 @@
 ﻿using System.Text.Json.Serialization;
-
 namespace KyivDigital.Business.Models
 {
     public class BaseResponse
     {
         [JsonPropertyName("message")]
         public string ErrorMessage { get; set; }
-        public bool IsSuccess => ErrorMessage == null;
+        public bool IsSuccess
+        {
+            get => isSuccess || ErrorMessage == null;
+            set => isSuccess = value;
+        }
+
+        private bool isSuccess;
     }
 }
