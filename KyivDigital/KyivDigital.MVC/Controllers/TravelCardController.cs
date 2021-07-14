763f0f1f29a757b235c4bb70ed83966033f45fbb
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-07-14 15:54:11 +0300

    Added pictures and design for travel cards

--- KyivDigital/KyivDigital.MVC/Controllers/TravelCardController.cs
+++ KyivDigital/KyivDigital.MVC/Controllers/TravelCardController.cs
@@ -1,5 +1,6 @@
 ﻿using KyivDigital.Business.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace KyivDigital.MVC.Controllers
@@ -15,6 +16,40 @@ namespace KyivDigital.MVC.Controllers
 
 
         //public IActionResult Index() => View();
-        public async Task<IActionResult> Index() => View((await _travelCardService.GetTravelCardsAsync()).Cards);
+        public async Task<IActionResult> Index()
+        {
+            var cards = await _travelCardService.GetTravelCardsAsync();
+
+            foreach (var card in cards.Cards)
+            {
+                card.Code = card.Code.Group();
+            }
+
+            return View(cards.Cards);
+        }
+
+
+        
+
+    }
+
+    public static class Dwe
+    {
+        public static string Group(this string s, int groupSize = 4, char groupSeparator = ' ')
+        {
+            var formattedIdentifierBuilder = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i != 0 && (s.Length - i) % groupSize == 0)
+                {
+                    formattedIdentifierBuilder.Append(groupSeparator);
+                }
+
+                formattedIdentifierBuilder.Append(s[i]);
+            }
+
+            return formattedIdentifierBuilder.ToString();
+        }
     }
 }
