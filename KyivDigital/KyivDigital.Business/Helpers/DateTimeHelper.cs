5c96bb341294a3f64d09d7c1cf853a8004e3ef6d
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-18 22:08:11 +0200

    Change dateTime formatter

--- KyivDigital/KyivDigital.Business/Helpers/DateTimeHelper.cs
+++ KyivDigital/KyivDigital.Business/Helpers/DateTimeHelper.cs
@@ -7,19 +7,20 @@ namespace KyivDigital.Business.Helpers
     {
         public static string GetTitleTime(DateTime dateTime)
         {
-            var today = DateTime.Today;
+            var today = DateTime.Now;
             var diff = today.Subtract(dateTime);
+            var time = dateTime.ToString("HH:mm");
             if (diff.Days == 0)
             {
                 if (today.Day == dateTime.Day)
-                    return "Сьогодні";
-                return "Вчора";
+                    return $"Сьогодні ({time})";
+                return $"Вчора ({time})";
             }
             if (diff.Days == 1)
-                return "Вчора";
-            if (diff.Days <= 365)
-                return dateTime.ToString("dd MMMM", CultureInfo.CreateSpecificCulture("uk"));
-            return dateTime.ToString("dd MMMM yyyy", CultureInfo.CreateSpecificCulture("uk"));
+                return $"Вчора ({time})";
+            if (dateTime.Year == today.Year)
+                return dateTime.ToString($"dd MMMM ({time})", CultureInfo.CreateSpecificCulture("uk"));
+            return dateTime.ToString($"dd MMMM yyyy ({time})", CultureInfo.CreateSpecificCulture("uk"));
         }
     }
 }
